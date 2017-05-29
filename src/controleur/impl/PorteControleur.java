2400712c3e4aab69ed2e002d334a0ecec3f13cc8
jeanmicheldu44 <jeux-max_v25@hotmail.fr>
2017-05-29 11:44:05 +0200

    MAJ controleur

--- src/controleur/impl/PorteControleur.java
+++ src/controleur/impl/PorteControleur.java
@@ -1,6 +1,31 @@
 package controleur.impl;
 
 import controleur.IPorteControleur;
+import modele.Capteur;
+import utils.EnumEtatCo;
 
 public class PorteControleur implements IPorteControleur{
+    public void enregistreContact(Capteur capteur) {
+
+    }
+
+    public void ouvre() {
+
+    }
+
+    public void ferme() {
+
+    }
+
+    public EnumEtatCo urgence() {
+        return EnumEtatCo.URGENCE;
+    }
+
+    public boolean contact(Capteur capteur) {
+        return true;
+    }
+
+    public void repriseCle() {
+
+    }
 }
