66ca50670dcda9f43174beb4ced88c4731c8be73
AntoineGrn <antoinegrn12@gmail.com>
2017-05-30 15:02:57 +0200

    Implémentation de la porte et du moteur

--- src/modele/Moteur.java
+++ src/modele/Moteur.java
@@ -3,32 +3,77 @@ package modele;
 import utils.EnumEtatM;
 
 public class Moteur {
-    private Long no_moteur;
+    private long no_moteur;
     private EnumEtatM etat_moteur;
 
+    /**
+     * Constructeur de la classe moteur
+     * @param no_moteur
+     * @param etat_moteur
+     */
     public Moteur(Long no_moteur, EnumEtatM etat_moteur) {
         this.no_moteur = no_moteur;
         this.etat_moteur = etat_moteur;
     }
 
-    public Long getNo_moteur() {
+    /**
+     * Récupère le numéro du moteur
+     * @return
+     */
+    public long getNo_moteur() {
         return no_moteur;
     }
 
-    public void setNo_moteur(Long no_moteur) {
+    /**
+     * Affecte un numéro au moteur
+     * @param no_moteur
+     */
+    public void setNo_moteur(long no_moteur) {
         this.no_moteur = no_moteur;
     }
 
+    /**
+     * Récupère l'état du moteur
+     * @return
+     */
     public EnumEtatM getEtat_moteur() {
         return etat_moteur;
     }
 
+    /**
+     * Affecte un état au moteur
+     * @param etat_moteur
+     */
     public void setEtat_moteur(EnumEtatM etat_moteur) {
         this.etat_moteur = etat_moteur;
     }
 
+    /**
+     * Le moteur tire la porte pour l'ouvrir
+     */
     public void tirer() {
+        this.etat_moteur = EnumEtatM.ENTIREE;
+    }
 
+    /**
+     * Le moteur pousse la porte pour la fermer
+     */
+    public void pousser() {
+        this.etat_moteur = EnumEtatM.ENPOUSSEE;
+    }
+
+    /**
+     * Le moteur appel la fonction d'arrêt
+     */
+    public void stop() {
+        arreter();
+    }
+
+    /**
+     * Arret du moteur
+     */
+    public void arreter() {
+        this.etat_moteur = EnumEtatM.ARRET;
     }
 
     @Override
--- src/modele/Porte.java
+++ src/modele/Porte.java
@@ -3,58 +3,147 @@ package modele;
 import utils.EnumEtatP;
 
 public class Porte {
-    private Long no_porte;
+    private long no_porte;
     private EnumEtatP etat_porte;
     private Moteur moteur;
+    private EnumEtatP etatPorteBloquee;
 
+    /**
+     * Constructeur de la classe Porte
+     * @param no_porte
+     * @param etat_porte
+     */
     public Porte(Long no_porte, EnumEtatP etat_porte) {
         this.no_porte = no_porte;
         this.etat_porte = etat_porte;
     }
 
-    public Long getNo_porte() {
+    /**
+     * Récupère le numéro de la porte
+     * @return
+     */
+    public long getNo_porte() {
         return no_porte;
     }
 
-    public void setNo_porte(Long no_porte) {
+    /**
+     * Affecte un numéro à la porte
+     * @param no_porte
+     */
+    public void setNo_porte(long no_porte) {
         this.no_porte = no_porte;
     }
 
+    /**
+     * Récupère l'état de la porte
+     * @return
+     */
     public EnumEtatP getEtat_porte() {
         return etat_porte;
     }
 
+    /**
+     * Affecte un nouvel état à la porte
+     * @param etat_porte
+     */
     public void setEtat_porte(EnumEtatP etat_porte) {
         this.etat_porte = etat_porte;
     }
 
+    /**
+     * Récupère l'instance du moteur
+     * @return
+     */
+    public Moteur getMoteur() {
+        return moteur;
+    }
+
+    /**
+     * Affecte un moteur à la porte
+     * @param moteur
+     */
+    public void setMoteur(Moteur moteur) {
+        this.moteur = moteur;
+    }
+
+    /**
+     * Récupère l'état de la porte avant qu'elle ne soit bloquée
+     * @return
+     */
+    public EnumEtatP getEtatPorteBloquee() {
+        return etatPorteBloquee;
+    }
+
+    /**
+     * Affecte un état à la porte avant qu'elle ne soit bloquée
+     * @param etatPorteBloquee
+     */
+    public void setEtatPorteBloquee(EnumEtatP etatPorteBloquee) {
+        this.etatPorteBloquee = etatPorteBloquee;
+    }
+
+    /**
+     * Permet d'ouvrir la porte
+     */
     public void ouvre() {
-        //moteur.
+        moteur.tirer();
+        this.etat_porte = EnumEtatP.ENOUVERTURE;
     }
 
+    /**
+     * Permet de fermer la porte
+     */
     public void ferme() {
-        //TODO
+        moteur.pousser();
+        this.etat_porte = EnumEtatP.ENFERMETURE;
     }
 
+    /**
+     * Arrête l'action de la porte
+     */
     public void pause() {
-        //TODO
+        moteur.stop();
+        this.etat_porte = EnumEtatP.ARRETEE;
     }
 
+    /**
+     * Passe l'état de la porte à fermée
+     */
     public void fermee() {
-        //TODO
+        this.etat_porte = EnumEtatP.FERMEE;
     }
 
+    /**
+     * Passe l'état de la porte à ouverte
+     */
     public void ouverte() {
-        //TODO
+        this.etat_porte = EnumEtatP.OUVERTE;
     }
 
+    /**
+     * Passe la porte dans l'état bloquée
+     * @return
+     */
     public EnumEtatP bloque() {
-        //TODO
-        return null;
+        this.etatPorteBloquee = this.etat_porte;
+        this.etat_porte = EnumEtatP.BLOQUEE;
+
+        return this.etatPorteBloquee;
     }
 
+    /**
+     * Reprise de la porte après un arrêt
+     * @param etat
+     */
     public void reprend(EnumEtatP etat) {
-        //TODO
+        this.etat_porte = etat;
+        if (etat == EnumEtatP.ENFERMETURE) {
+            moteur.pousser();
+        } else if (etat == EnumEtatP.ENOUVERTURE) {
+            moteur.tirer();
+        } else {
+            //Problème : avant que la porte se bloque, son état n'est pas bien enregistré
+        }
     }
 
     @Override
